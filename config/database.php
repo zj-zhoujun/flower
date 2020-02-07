480756aba55eee992e742da5192a357214256ca9
周军 <254575481@qq.com>
2020-02-07 15:38:21 +0800

    数据库配置文件

--- config/database.php
+++ config/database.php
@@ -11,13 +11,21 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
+//    'hostname'        => '127.0.0.1',
+//    // 数据库名
+//    'database'        => 'flower',
+//    // 用户名
+//    'username'        => 'root',
+//    // 密码
+//    'password'        => 'root',
+    // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'flower',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'root',
+    'password'        => '25zjjianjian!',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
